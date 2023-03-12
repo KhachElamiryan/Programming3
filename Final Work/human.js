ce1ae65a02d09db2d4924d42d46312e78af08895
KhachElamiryan <khachik.e@tumo.org>
2023-03-12 12:08:19 +0400

    first commit

---
+++ Final Work/human.js
@@ -0,0 +1,140 @@
+class Human {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 10;
+        this.directions = [];
+
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        let emptyCelss = this.chooseCell(0)
+        let newCell = random(emptyCelss)
+        if (newCell) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[newY][newX] = 4
+            let newGr = new Human(newX, newY)
+             HumanArr.push(newGr)
+            this.energy = 10
+        }
+    }
+
+    move() {
+        this.energy--
+        let emptyCelss = this.chooseCell(0)
+        let newCell = random(emptyCelss)
+        if (newCell && this.energy >= 0) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]///////2
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        } else {
+            this.die()
+        }
+    }
+
+    eat() {
+        let emptyCelss = this.chooseCell(2)
+        let newCell = random(emptyCelss)
+        let emptyCelss1 = this.chooseCell(3)
+        let newCell1 = random(emptyCelss1)
+        let emptyCelss2 = this.chooseCell(1)
+        let newCell2 = random(emptyCelss2)
+        if (newCell) {
+            this.energy++
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]///////2
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            if (this.energy >= 25) {
+                this.mul()
+            }
+            for (var i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1);
+                    break;
+                }
+            }
+
+        } else if (newCell1) {
+            this.energy++
+            let newX = newCell1[0]
+            let newY = newCell1[1]
+            matrix[newY][newX] = matrix[this.y][this.x]///////2
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            if (this.energy >= 25) {
+                this.mul()
+            }
+            for (var i in PredatorArr) {
+                if (newX == PredatorArr[i].x && newY == PredatorArr[i].y) {
+                    PredatorArr.splice(i, 1);
+                    break;
+                }
+            }
+        }
+        else if (newCell2) {
+            this.energy++
+            let newX = newCell2[0]
+            let newY = newCell2[1]
+            matrix[newY][newX] = matrix[this.y][this.x]///////2
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            if (this.energy >= 25) {
+                this.mul()
+            }
+            for (var i in grassArr) {
+                if (newX == grassArr[i].x && newY == grassArr[i].y) {
+                    grassArr.splice(i, 1);
+                    break;
+                }
+            }
+        } else {
+            this.move()
+        }
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0
+        for (var i in HumanArr) {
+            if (this.x == HumanArr[i].x && this.y == HumanArr[i].y) {
+                HumanArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
---
+++ Final Work/script.js
@@ -0,0 +1,142 @@
+
+
+function generateMatrix(MatrixLength, gr, grEater, predator, human, killer) {
+    let matrix = []
+    for (let i = 0; i < MatrixLength; i++) {
+        matrix.push([])
+        for (let j = 0; j < MatrixLength; j++) {
+            matrix[i].push(0)
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * MatrixLength)
+        let y = Math.floor(Math.random() * MatrixLength)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 1
+        }
+    }
+    for (let i = 0; i < grEater; i++) {
+        let x = Math.floor(Math.random() * MatrixLength)
+        let y = Math.floor(Math.random() * MatrixLength)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 2
+        }
+
+    }
+    for (let i = 0; i < predator; i++) {
+        let x = Math.floor(Math.random() * MatrixLength)
+        let y = Math.floor(Math.random() * MatrixLength)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 3
+        }
+
+
+    }
+    for (let i = 0; i < human; i++) {
+        let x = Math.floor(Math.random() * MatrixLength)
+        let y = Math.floor(Math.random() * MatrixLength)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 4
+        }
+
+
+    }
+    for (let i = 0; i < killer; i++) {
+        let x = Math.floor(Math.random() * MatrixLength)
+        let y = Math.floor(Math.random() * MatrixLength)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 5
+        }
+
+
+    }
+
+    return matrix
+
+}
+
+
+
+
+
+
+let matrix = generateMatrix(60, 100, 100, 90, 5, 50)
+var side = 15;
+let grassArr = []
+let grassEaterArr = []
+let PredatorArr = []
+let HumanArr = []
+let KillerArr = []
+
+
+function setup() {
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let gr = new Grass(x, y)
+                grassArr.push(gr)
+            } else if (matrix[y][x] == 2) {
+                let gr = new GrassEater(x, y)
+                grassEaterArr.push(gr)
+            } else if (matrix[y][x] == 3) {
+                let gr = new Predator(x, y)
+                PredatorArr.push(gr)
+            }
+            else if (matrix[y][x] == 4) {
+                let gr = new Human(x, y)
+                HumanArr.push(gr)
+            }
+            else if (matrix[y][x] == 5) {
+                let gr = new Killer(x, y)
+                KillerArr.push(gr)
+            }
+
+        }
+    }
+
+}
+
+function draw() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+            } else if (matrix[y][x] == 0) {
+                fill("#acacac");
+            } else if (matrix[y][x] == 2) {
+                fill("yellow");
+            } else if (matrix[y][x] == 3) {
+                fill("red");
+            }
+            else if (matrix[y][x] == 4) {
+                fill("blue");
+            }
+            else if (matrix[y][x] == 5) {
+                fill("black");
+            }
+
+            rect(x * side, y * side, side, side);
+        }
+    }
+    for (let i = 0; i < grassArr.length; i++) {
+        grassArr[i].mul()
+    }
+
+    for (let i = 0; i < grassEaterArr.length; i++) {
+        grassEaterArr[i].eat()
+    } for (let i = 0; i < PredatorArr.length; i++) {
+        PredatorArr[i].eat()
+    }
+    for (let i = 0; i < HumanArr.length; i++) {
+        HumanArr[i].eat()
+    }
+    for (let i = 0; i < KillerArr.length; i++) {
+        KillerArr[i].eat()
+    }
+
+}
